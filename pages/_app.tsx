1bf83eeb0904dfecc1895625da8e7bcf3d28a43d
charlesmccarthy <129569840+charlesmccarthy@users.noreply.github.com>
2023-12-12 23:51:58 -0500

    edit main

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -9,10 +9,7 @@ import "react-toastify/dist/ReactToastify.css";
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-      <main
-        className="absolute top-0 bottom-0 left-0 right-0 min-h-screen max-h-screen"
-        style={{ backgroundImage: 'url("bg.svg")' }}
-      >
+      <main className="absolute top-0 bottom-0 left-0 right-0 min-h-screen max-h-screen">
         <Component {...pageProps} />
       </main>
       <ToastContainer />
